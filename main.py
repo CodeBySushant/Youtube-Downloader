a224fe6eb26e03b74954b3b6454756839ad03e25
Sushant Sharma <itsofficialsushant@gmail.com>
2025-08-23 21:36:06 +0100

    Youtube Video Downloader Code

---
+++ main.py
@@ -0,0 +1,8 @@
+from pytube import YouTube
+
+link = input("Enter The URL of Video: ")
+video = YouTube(link)
+stream = video.streams.get_highest_resolution()
+stream.download()
+print("✅ Download complete!")
+
